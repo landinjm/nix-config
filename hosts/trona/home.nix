4dbb6079afe1b4e1cd97f893306b69a72274c1ab
landinjm <landinjm@umich.edu>
2026-07-29 23:40:17 -0400

    okie

--- hosts/trona/home.nix
+++ hosts/trona/home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, inputs, ... }:
+{ config, lib, pkgs, inputs, ... }:
 
 {
   home.username = "trona";
@@ -9,11 +9,16 @@
   # User packages
   home.packages = with pkgs; [
     neovim
-    ripgrep
-    fd
-    tree
-    bat
-    eza
+    cmakeWithGui
+    gcc
+    openmpi
+    python3
+    clang-tools
+    openblas
+    boost
+    scalapack
+    symengine
+    hdf5-mpi
   ];
 
   xdg.configFile."ghostty/config".text = ''
@@ -45,6 +50,76 @@
 
     settings.user.name = "landinjm";
     settings.user.email = "landinjm@umich.edu";
+    settings.init.defaultBranch = "main";
+    settings.push.autoSetupRemote = true;
+  };
+
+  programs.zoxide = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.eza = {
+    enable = true;
+    icons = "auto";
+    extraOptions = [
+      "--group-directories-first"
+      "--no-quotes"
+      "--icons=always"
+    ];
+  };
+
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = true;
+      format = lib.concatStrings [
+        "$nix_shell"
+        "$hostname"
+        "$directory"
+        "$git_branch"
+        "$git_state"
+        "$git_status"
+        "$character"
+      ];
+      directory = {style = "090E13";};
+
+      character = {
+        success_symbol = "[❯](090E13)";
+        error_symbol = "[❯](red)";
+        vimcmd_symbol = "[❮](cyan)";
+      };
+
+      nix_shell = {
+        format = "[$symbol]($style) ";
+        symbol = "🐚";
+        style = "";
+      };
+
+      git_branch = {
+        symbol = "[](12171E) ";
+        style = "fg:090E13 bg:12171E";
+        format = "on [$symbol$branch]($style)[](12171E) ";
+      };
+
+      git_status = {
+        format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](218)($ahead_behind$stashed)]($style)";
+        style = "cyan";
+        conflicted = "";
+        renamed = "";
+        deleted = "";
+        stashed = "≡";
+      };
+
+      git_state = {
+        format = "([$state( $progress_current/$progress_total)]($style)) ";
+        style = "bright-black";
+      };
+    };
+  };
+
+  programs.bash = {
+  enable = true;
   };
 
   # Let Home Manager manage itself
